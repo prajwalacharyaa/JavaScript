7c6525c4d7ba4697412209c5ba7acf69a0c1aec8
AnanymousAch <35920575+AnanymousAch@users.noreply.github.com>
2024-05-23 11:48:07 +0545

    search

---
+++ search.js
@@ -0,0 +1,5 @@
+let text = "This is the text that needs to be scanned. Can you scan and find the name de7mber?";
+let n = text.search("de7mber");
+console.log(n);
+let mango =text.replace("de7mber","november");
+console.log(mango);
